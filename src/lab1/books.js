16fc3ad845ca69048ffa78702a5edc1c124d9399
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2021-12-22 23:46:28 +0600

    lab 1 task going to submit

--- src/lab1/books.js
+++ src/lab1/books.js
@@ -14,4 +14,32 @@ export const books = [
       img: "https://images-na.ssl-images-amazon.com/images/I/51MB5SD5GIL._AC_UL200_SR200,200_.jpg",
       upvote: 4
     },
+    {
+      id: 3,
+      title: "Atlas of the Heart: Mapping Meaningful Connection and the Language of Human Experience ",
+      author: "Brené Brown ",
+      img: "https://images-na.ssl-images-amazon.com/images/I/51KvAoff+ZL._SX407_BO1,204,203,200_.jpg",
+      upvote: 4
+    },
+    {
+      id: 4,
+      title: "Will",
+      author: "Will Smith",
+      img: "https://m.media-amazon.com/images/I/61gS6EWmWwL.jpg",
+      upvote: 4
+    },
+    {
+      id: 5,
+      title: "Life Skills for Teens: How to Cook, Clean, Manage Money, Fix Your Car, Perform First Aid, and Just About Everything in Between",
+      author: "Karen Harris",
+      img: "https://images-na.ssl-images-amazon.com/images/I/41iWUx2mwdL._SX331_BO1,204,203,200_.jpg",
+      upvote: 4
+    },
+    {
+      id: 6,
+      title: "Interesting Stories For Curious People: A Collection of Fascinating Stories About History, Science, Pop Culture and Just About Anything Else You Can Think of",
+      author: "Bill O'Neill",
+      img: "https://images-na.ssl-images-amazon.com/images/I/51csugYPrJL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+      upvote: 4
+    },
   ];
