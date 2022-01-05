f04c0f1e27847b6b2f5633d75978a5d79d31a5f4
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2022-01-05 12:57:31 +0600

    basic setup

--- src/index.js
+++ src/index.js
@@ -1,66 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import Book from './lab1/Book'
-import {books} from './lab1/books'
-import "./lab1/book.css";
-
-function compare( a, b ) {
-  if ( a.upvote > b.upvote ){
-    return -1;
-  }
-  if ( a.upvote < b.upvote ){
-    return 1;
-  }
-  return 0;
-}
 
 
-
-
-
-function Booklist() {
-  const [booklist,setBooklist] = useState(books.sort(compare))
-  console.log(booklist)
-
-
-
-const updateVote = (id) => {
-    var updatedBooks = booklist.map((book) => book.id === id ? {
-      ...book,
-      upvote: book.upvote +1
-    } : book);
-
-    var updatedBooksSort = updatedBooks.sort(compare)
-
-
-    setBooklist(updatedBooksSort)
+function App() {
   
-  }
   return (
-    <section className="booklist">
+    <section>
       
-      {booklist.map((book) => {
-        
-        return (
-          <>
-          <Book key={book.id} {...book}>
-          
-          </Book>
-          <button className='button-8' onClick={() => updateVote(book.id)}>
-            upvote
-          </button>
-          
-          </>
-        
-        
-        );
-      })}
+      <h2>Welcome to react</h2>
     </section>
   );
 }
 
 
-ReactDOM.render(<Booklist />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 
 
